5e951a05dce845510f728bd1e5374b0a129b26d1
Nada Shaban <nadashaban971@gmail.com>
2026-01-14 14:12:37 +0200

    project initialization and connect to postgres

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import type {Request, Response} from "express"
+import pool from './src/config/db.js';
+
+const app = express();
+app.use(express.json());
+const PORT = 5000;
+
+app.get('/test-db', async (req: Request, res: Response) => {
+    try {
+      const result = await pool.query('SELECT NOW()');
+      res.json({ message: "Connected to Postgres!", time: result.rows[0] });
+    } catch (err) {
+      console.error(err);
+      res.status(500).send("Database connection error");
+    }
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
