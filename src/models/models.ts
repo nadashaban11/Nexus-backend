0256e01a7995a5480172675dc02740d02da2e823
Nada Shaban <nadashaban971@gmail.com>
2026-01-15 15:00:20 +0200

    Add types

---
+++ src/models/models.ts
@@ -0,0 +1,31 @@
+export interface User {
+    id: number;
+    first_name: string;
+    last_name: string;
+    user_name: string;
+    email: string;
+    password: string; 
+    created_at: Date;
+}
+
+export interface Post {
+    id: number;
+    title: string;
+    content: string | null; 
+    url: string;
+    user_id: number;
+    created_at: Date;
+    updated_at: Date;
+    user_name?: string; 
+}
+
+export interface Comment {
+    id: number;
+    post_id: number;
+    user_id: number;
+    content: string;
+    created_at: Date;
+    updated_at: Date;
+    user_name?: string; 
+}
+
